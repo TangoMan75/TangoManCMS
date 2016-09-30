7958484ce448a0834b2390916cd4870f679f2f07
Matthias Morin <matthias.morin@gmail.com>
2016-09-30 09:29:08 +0200

    Some Cleaning

--- clear_cache_prod.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-title Clear prod cache
-echo php bin/console cache:clear --env=prod
-echo.
-echo.
-echo.
-php bin/console cache:clear --env=prod
-pause
--- run.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-title Run symfony server
-echo Run symfony server
-echo.
-echo.
-echo.
-start http://localhost:8000
-php bin/console server:run
-pause
