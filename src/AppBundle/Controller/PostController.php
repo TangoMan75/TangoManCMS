b23bc8ebaa9689d9b960a15976a6a5ecc89b14dd
Matthias Morin <matthias.morin@gmail.com>
2016-10-05 17:49:30 +0200

    Edit & Delete Post With Admin Rights

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,31 +11,56 @@ use Symfony\Component\HttpFoundation\Request;
 class PostController extends Controller
 {
     /**
-     * @Route("/edit/{id}", name="post_edit")
+     * @Route("/edit/{id}", requirements={"id": "\d+"}, name="post_edit")
      */
     public function editAction(Request $request, Post $post)
     {
-        return $this->render('post/edit.html.twig', [
-            // ...
-        ]);
+        if (!$post) {
+
+            $this->get('session')->getFlashBag()->add('error', "Ce message n'existe pas.");
+
+        } else {
+
+            $form = $this->createForm(PostType::class, $post);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $this->get('em')->save($post);
+
+                $this->get('session')->getFlashBag()->add('success', "Votre message <strong>&quot;{$post->getTitle()}&quot</strong> à bien été modifié.");
+
+                return $this->redirectToRoute('app_homepage');
+
+            } else {
+
+                return $this->render('post/edit.html.twig', [
+                    "form_post" => $form->createView()
+                ]);
+            }
+        }
     }
 
     /**
-     * @Route("/delete/{id}", name="post_delete")
+     * @Route("/delete/{id}", requirements={"id": "\d+"}, name="post_delete")
      */
     public function deleteAction(Request $request, Post $post)
     {
         if (!$post) {
+
             $this->get('session')->getFlashBag()->add('error', "Ce message n'existe pas.");
+
         } else {
 
             // Deletes specified post
             $this->get('em')->remove($post);
             $this->get('em')->flush();
 
-            $this->get('session')->getFlashBag()->add('success', "Le message &quot;{$post->getTitle()}&quot; à été supprimé.");
+            $this->get('session')->getFlashBag()->add('success', "Le message <strong>&quot;{$post->getTitle()}&quot;</strong> à été supprimé.");
         }
+
         return $this->redirectToRoute('app_homepage');
+
     }
 
 }
