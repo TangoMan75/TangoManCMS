499188a5c91cb4b30d4329cb4f1d9c1d251ca3e1
Matthias Morin <matthias.morin@gmail.com>
2016-10-06 16:55:54 +0200

    Clean Post Controller

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -4,8 +4,8 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
 use AppBundle\Form\PostType;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -23,18 +23,18 @@ class PostController extends Controller
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
 
             $user = $this->getUser();
-
             $post = new Post();
             $post->setUser($user);
-
             $form = $this->createForm(PostType::class, $post);
             $form->handleRequest($request);
             $formPost = $form->createView();
 
-            if ($form->isValid()) {
+            if ( $form->isValid() ) {
+
                 $this->get('em')->save($post);
                 $this->get('session')->getFlashBag()->add('success', 'Votre message a bien été enregistré.');
                 return $this->redirectToRoute('app_homepage');
+
             }
         }
 
@@ -48,39 +48,35 @@ class PostController extends Controller
      */
     public function editAction(Request $request, Post $post)
     {
-        if (!$this->getUser()) {
+        if ( !$this->getUser() ) {
 
             $this->get('session')->getFlashBag()->add('error', "Vous devez être connecté pour pouvoir éditer des messages.");
             return $this->redirectToRoute('app_homepage');
 
-        } else {
+        }
 
-            if ( $this->getUser() !== $post->getUser() && !in_array('ROLE_ADMIN', $this->getUser()->getRoles()) ) {
+        if ( $this->getUser() !== $post->getUser() && !in_array('ROLE_ADMIN', $this->getUser()->getRoles()) ) {
 
-                $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à éditer ce message.");
-                return $this->redirectToRoute('app_homepage');
-
-            }
-
-            $form = $this->createForm(PostType::class, $post);
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-
-                $this->get('em')->save($post);
-
-                $this->get('session')->getFlashBag()->add('success', "Votre message <strong>&quot;{$post->getTitle()}&quot</strong> à bien été modifié.");
-
-                return $this->redirectToRoute('app_homepage');
-
-            } else {
-
-                return $this->render('post/edit.html.twig', [
-                    "form_post" => $form->createView()
-                ]);
-            }
+            $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à éditer ce message.");
+            return $this->redirectToRoute('app_homepage');
 
         }
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ) {
+
+            $this->get('em')->save($post);
+            $this->get('session')->getFlashBag()->add('success', "Votre message <strong>&quot;{$post->getTitle()}&quot</strong> à bien été modifié.");
+            return $this->redirectToRoute('app_homepage');
+
+        }
+
+        return $this->render('post/edit.html.twig', [
+            "form_post" => $form->createView()
+        ]);
+
     }
 
     /**
@@ -88,21 +84,17 @@ class PostController extends Controller
      */
     public function deleteAction(Request $request, Post $post)
     {
-        if ( $this->getUser() == $post->getUser() || in_array('ROLE_ADMIN', $this->getUser()->getRoles()) ) {
-
-            // Deletes specified post
-            $this->get('em')->remove($post);
-            $this->get('em')->flush();
-
-            $this->get('session')->getFlashBag()->add('success', "Le message <strong>&quot;{$post->getTitle()}&quot;</strong> à été supprimé.");
-
-        } else {
+        if ( $this->getUser() !== $post->getUser() && !in_array( 'ROLE_ADMIN', $this->getUser()->getRoles() ) ) {
 
             $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à supprimer ce message.");
             return $this->redirectToRoute('app_homepage');
 
         }
 
+        // Deletes specified post
+        $this->get('em')->remove($post);
+        $this->get('em')->flush();
+        $this->get('session')->getFlashBag()->add('success', "Le message <strong>&quot;{$post->getTitle()}&quot;</strong> à été supprimé.");
         return $this->redirectToRoute('app_homepage');
     }
 
