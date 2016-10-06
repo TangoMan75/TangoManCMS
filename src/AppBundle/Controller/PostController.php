94c3c073bcff746b494253217b7cb11458f164f9
Matthias Morin <matthias.morin@gmail.com>
2016-10-06 14:37:29 +0200

    Some Front-End improvement + newPost Controller

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -14,6 +14,36 @@ use Symfony\Component\HttpFoundation\Request;
 class PostController extends Controller
 {
     /**
+     * @Route("/new", name="post_new")
+     */
+    public function newAction(Request $request)
+    {
+        $formPost = null;
+
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+
+            $user = $this->getUser();
+
+            $post = new Post();
+            $post->setUser($user);
+
+            $form = $this->createForm(PostType::class, $post);
+            $form->handleRequest($request);
+            $formPost = $form->createView();
+
+            if ($form->isValid()) {
+                $this->get('em')->save($post);
+                $this->get('session')->getFlashBag()->add('success', 'Votre message a bien été enregistré.');
+                return $this->redirectToRoute('app_homepage');
+            }
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'form_post' => $formPost
+        ]);
+    }
+
+    /**
      * @Route("/edit/{id}", requirements={"id": "\d+"}, name="post_edit")
      */
     public function editAction(Request $request, Post $post)
--- web/js/livredor.js
+++ web/js/livredor.js
@@ -1,3 +1,15 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(function() {
+    $("a.smooth").on("click", function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000, function() {
+            window.location.hash = hash;
+        })
+    })
+})
