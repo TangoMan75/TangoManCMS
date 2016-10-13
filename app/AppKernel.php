40b43e10d819586e8e22d7b3f8e8aad0b886f7cf
Matthias Morin <matthias.morin@gmail.com>
2016-10-13 18:16:29 +0200

    :tada: install base64 bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,20 +8,21 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Tiloweb\PaginationBundle\TilowebPaginationBundle(),
             new AppBundle\AppBundle(),
-            new Symfony\Bundle\DebugBundle\DebugBundle(),
-            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
-            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+            new Symfony\Bundle\DebugBundle\DebugBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+            new Tiloweb\Base64Bundle\TilowebBase64Bundle(),
+            new Tiloweb\PaginationBundle\TilowebPaginationBundle(),
         ];
 
         return $bundles;
