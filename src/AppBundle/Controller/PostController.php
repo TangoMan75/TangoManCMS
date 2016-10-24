cecaf743e955a326652837a6207018de183cfe5a
Matthias Morin <matthias.morin@gmail.com>
2016-10-24 15:20:06 +0200

    Updated PostController

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -30,7 +30,7 @@ class PostController extends Controller
         $formComment = null;
 
         // User cannot comment when not logged in
-        if ( $this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ) {
+        if ( $this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED') ) {
 
             $user = $this->getUser();
             $comment = new Comment();
@@ -41,16 +41,13 @@ class PostController extends Controller
             $formComment = $form->createView();
 
             if ( $form->isSubmitted() && $form->isValid() ) {
-
                 $this->get('em')->save($comment);
                 $this->get('session')->getFlashBag()->add('success', 'Votre commentaire a bien été enregistré.');
 
                 // User is redirected to referrer page
                 return $this->redirectToRoute('app_homepage');
-
             }
         }
-
         return $this->render('post/index.html.twig', [
             'form_post'    => $formComment,
             'list_comment' => $listComment,
@@ -68,7 +65,7 @@ class PostController extends Controller
         $formPost = null;
 
         // User cannot post when not logged in
-        if ( $this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ) {
+        if ( $this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED') ) {
 
             $user = $this->getUser();
             $post = new Post();
@@ -78,16 +75,13 @@ class PostController extends Controller
             $formPost = $form->createView();
 
             if ( $form->isSubmitted() && $form->isValid() ) {
-
                 $this->get('em')->save($post);
                 $this->get('session')->getFlashBag()->add('success', 'Votre message a bien été enregistré.');
 
                 // User is redirected to referrer page
                 return $this->redirect( $request->get('callback') );
-
             }
         }
-
         return $this->render('post/edit.html.twig', [
             'form_post' => $formPost
         ]);
@@ -100,21 +94,16 @@ class PostController extends Controller
      */
     public function editAction(Request $request, Post $post)
     {
-        // User cannot edit when not logged in
-        if ( !$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ) {
-
-            $this->get('session')->getFlashBag()->add('error', "Vous devez être connecté pour pouvoir éditer des ".
-                                                               "messages.");
+        // User must log in
+        if ( !$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED') ) {
+            $this->get('session')->getFlashBag()->add('error', "Vous devez être connecté pour réaliser cette action.");
             return $this->redirectToRoute('app_login');
-
         }
 
         // Only author or admin can edit post
-        if ( $this->getUser() !== $post->getUser() && !in_array('ROLE_ADMIN', $this->getUser()->getRoles()) ) {
-
-            $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à éditer ce message.");
+        if ( $this->getUser() !== $comment->getUser() && !in_array('ROLE_ADMIN', $this->getUser()->getRoles()) ) {
+            $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à réaliser cette action.");
             return $this->redirectToRoute('app_homepage');
-
         }
 
         $form = $this->createForm(PostType::class, $post);
@@ -123,15 +112,12 @@ class PostController extends Controller
         if ( $form->isSubmitted() && $form->isValid() ) {
 
             $this->get('em')->save($post);
-            $this->get('session')->getFlashBag()->add('success', "Votre message <strong>".
-                                                                 "&quot;{$post->getTitle()}&quot</strong> ".
-                                                                 "à bien été modifié.");
-
+            $this->get('session')->getFlashBag()->add('success',
+                "Votre message <strong>&quot;{$post->getTitle()}&quot</strong> à bien été modifié."
+            );
             // User is redirected to referrer page
             return $this->redirect( $request->get('callback') );
-
         }
-
         return $this->render('post/edit.html.twig', [
             'form_post' => $form->createView()
         ]);
@@ -145,22 +131,25 @@ class PostController extends Controller
      */
     public function deleteAction(Request $request, Post $post)
     {
+        // User must log in
+        if ( !$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED') ) {
+            $this->get('session')->getFlashBag()->add('error', "Vous devez être connecté pour réaliser cette action.");
+            return $this->redirectToRoute('app_login');
+        }
+
         // Only author or admin can delete post
-        if ( $this->getUser() !== $post->getUser() && !in_array( 'ROLE_ADMIN', $this->getUser()->getRoles() ) ) {
-
-            $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à supprimer ce message.");
+        if ( $this->getUser() !== $comment->getUser() && !in_array('ROLE_ADMIN', $this->getUser()->getRoles()) ) {
+            $this->get('session')->getFlashBag()->add('error', "Vous n'êtes pas autorisé à réaliser cette action.");
             return $this->redirectToRoute('app_homepage');
-
         }
 
         // Deletes specified post
         $this->get('em')->remove($post);
         $this->get('em')->flush();
-        $this->get('session')->getFlashBag()->add('success', "Le message <strong>&quot;".
-                                                             "{$post->getTitle()}&quot;</strong> à été supprimé.");
-
+        $this->get('session')->getFlashBag()->add('success',
+            "Le message <strong>&quot;{$post->getTitle()}&quot;</strong> à été supprimé."
+        );
         // User is redirected to referrer page
         return $this->redirect( $request->get('callback') );
     }
-
 }
