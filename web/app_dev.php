6af8b4d3e5a0e9cf5094faa9796c1a1ea4fcbd5b
Matthias Morin <matthias.morin@gmail.com>
2017-02-26 20:36:06 +0100

    App_Dev.php home IP

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,8 +11,8 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (
-    !in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1', '62.23.96.115'])
-    && !in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], ['62.23.96.115'])
+    !in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1', '62.23.96.115', '88.191.206.182'])
+    && !in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], ['62.23.96.115', '88.191.206.182'])
 ) {
     header('location: /');
     exit();
