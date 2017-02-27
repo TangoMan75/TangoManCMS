08ea7a3ae45b0d4a4da99f713ce853015d292fa1
Matthias Morin <matthias.morin@gmail.com>
2017-02-27 21:19:09 +0100

    UploadFileType

--- src/AppBundle/Form/CommentType.php
+++ src/AppBundle/Form/CommentType.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Form;
 
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
---
+++ src/AppBundle/Form/FileUploadType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class FileUploadType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('Fichier', FileType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new File([
+//                        'maxSize' => '1024k',
+//                        'maxSizeMessage' => "Le fichier que vous tentez d'importer est trop volumineux",
+                        'mimeTypes' => 'application/vnd.ms-excel',
+                        'mimeTypesMessage' => 'Vous ne pouvez importer que des fichiers de type CSV'
+                    ]),
+                ]
+            ]
+        )
+    }
+
+    public function getName()
+    {
+        return 'fileupload';
+    }
+}
