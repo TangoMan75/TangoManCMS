d923aefe40c03fbec616f08700f1c4625b6f4c67
Matthias Morin <matthias.morin@gmail.com>
2017-03-08 16:28:03 +0100

    Post import export

--- src/AppBundle/Controller/Admin/PostController.php
+++ src/AppBundle/Controller/Admin/PostController.php
@@ -4,10 +4,12 @@ namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\Post;
 use AppBundle\Form\EditPostType;
+use AppBundle\Form\FileUploadType;
 use AppBundle\Form\NewPostType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class PostController
@@ -118,4 +120,135 @@ class PostController extends Controller
         return $this->redirect($request->get('callback'));
     }
 
+    /**
+     * @Route("/export")
+     */
+    public function exportAction(Request $request)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $posts = $em->getRepository('AppBundle:Post')->findAll();
+
+        return $this->render(
+            'admin/post/export.html.twig',
+            [
+                'currentUser' => $this->getUser(),
+                'posts'       => $posts,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/export-json")
+     */
+    public function exportJSONAction(Request $request)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $posts = $em->getRepository('AppBundle:Post')->findAllPosts();
+        $response = json_encode($posts);
+
+        return new Response(
+            $response, 200, [
+                'Content-Type'        => 'application/force-download',
+                'Content-Disposition' => 'attachment; filename="posts.json"',
+            ]
+        );
+    }
+
+    /**
+     * @Route("/import")
+     */
+    public function importAction(Request $request)
+    {
+        $form = $this->createForm(FileUploadType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            $file = $request->files->get('file_upload')['file'];
+
+            if (!$file->isValid()) {
+                // Upload success check
+                $this->get('session')->getFlashBag()->add(
+                    'error',
+                    'Une erreur s\'est produite lors du transfert.<br/>Veuillez réessayer.'
+                );
+
+                return $this->redirectToRoute('app_admin_post_import');
+            }
+
+            return $this->importJSON($file);
+        }
+
+        return $this->render(
+            'admin/post/import.html.twig',
+            [
+                'currentUser' => $this->getUser(),
+                'form'        => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @param $file
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function importJSON($file)
+    {
+        // Security checks
+        $clientExtension = $file->getClientOriginalExtension();
+        if ($file->getClientMimeType() !== 'application/json' && !in_array($clientExtension, ['json']) ) {
+            $this->get('session')->getFlashBag()->add('error', 'Ce format du fichier n\'est pas supporté.');
+            return $this->redirectToRoute('app_admin_post_import');
+        }
+
+        $counter = 0;
+        $dupes   = 0;
+        // File check
+        if (is_file($file)) {
+            // Load post entity
+            $em = $this->get('doctrine')->getManager();
+            $posts = $em->getRepository('AppBundle:Post');
+            $users = $em->getRepository('AppBundle:User');
+
+            foreach (json_decode(file_get_contents($file)) as $import) {
+
+                // die(dump($import));
+
+                // Check if post exists already
+                if (!$posts->findOneBySlug($import->post_slug)) {
+                    $counter++;
+
+                    // Check if post author exists
+                    $user = $users->findOneByEmail($import->user_email);
+                    // When author doesn't exist sets current user as author
+                    if (!$user) {
+                        $user = $this->getUser();
+                    }
+
+                    $post = new Post();
+                    $post->setUser($user)
+                         ->setTitle($import->post_title)
+                         ->setSubtitle($import->post_subtitle)
+                         ->setSlug($import->post_slug)
+                         ->setContent($import->post_content);
+
+                    // $post->setDateCreated($import->post_dateCreated);
+
+                    $em->persist($post);
+                    $em->flush();
+                }
+            }
+
+            if ($counter > 1) {
+                $success = "$counter aticles ont été importés.";
+            } else {
+                $success = "Aucun article n'a été importé.";
+            }
+
+            $this->get('session')->getFlashBag()->add('success', $success);
+        }
+
+        return $this->redirectToRoute('app_admin_post_index');
+    }
 }
