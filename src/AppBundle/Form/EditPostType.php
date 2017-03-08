2bf2ae3177c8c1f6008e59ae3827f1ec8a78751d
Matthias Morin <matthias.morin@gmail.com>
2017-03-08 01:18:21 +0100

    Forms

--- src/AppBundle/Form/EditPostType.php
+++ src/AppBundle/Form/EditPostType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AdminPostType extends AbstractType
+class EditPostType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
