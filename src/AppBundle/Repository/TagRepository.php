6a718910d401b8b5d498ac280591b3dcdd7838e6
Matthias Morin <matthias.morin@gmail.com>
2017-03-08 16:28:35 +0100

    Tags

--- src/AppBundle/Repository/TagRepository.php
+++ src/AppBundle/Repository/TagRepository.php
@@ -4,4 +4,31 @@ namespace AppBundle\Repository;
 
 class TagRepository extends \Doctrine\ORM\EntityRepository
 {
+    // /**
+    //  * @param  string $name
+    //  *
+    //  * @return array
+    //  */
+    // public function findByName($name)
+    // {
+    //     return $this->createQueryBuilder('tag')
+    //                 ->where('tag.name = :name')
+    //                 ->setParameter('name', $name)
+    //                 ->getQuery()
+    //                 ->getResult();
+    // }
+
+    // /**
+    //  * @param  string $type
+    //  *
+    //  * @return array
+    //  */
+    // public function findByName($type)
+    // {
+    //     return $this->createQueryBuilder('tag')
+    //                 ->where('tag.type = :type')
+    //                 ->setParameter('type', $type)
+    //                 ->getQuery()
+    //                 ->getResult();
+    // }
 }
