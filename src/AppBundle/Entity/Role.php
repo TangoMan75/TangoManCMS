2de5bea23da67335fce89143448aa1074d05eed8
Matthias Morin <matthias.morin@gmail.com>
2017-03-13 16:48:06 +0100

    Role

---
+++ src/AppBundle/Entity/Role.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Role
+ * @ORM\Table(name="role")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RoleRepository")
+ */
+class Role
+{
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var User[]
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User", mappedBy="roles")
+     */
+    private $users;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get users
+     *
+     * @return Post[]|array
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * Set users
+     *
+     * @return Role
+     */
+    public function addUser($user)
+    {
+        $this->users[] = $user;
+
+        return $this;
+    }
+
+    /**
+     * Remove user
+     *
+     * @param $user
+     *
+     * @return Role
+     */
+    public function removeUser($user)
+    {
+        $this->users->removeElement($user);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+}
