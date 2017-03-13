fe5e8859a1c187fdb4af6646c01d9395c89f7a18
Matthias Morin <matthias.morin@gmail.com>
2017-03-13 13:49:04 +0100

    List 20 default

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -20,7 +20,7 @@ class UserRepository extends EntityRepository implements UserLoaderInterface
     {
         // Sets default values
         $page  = $query->get('page', 1);
-        $limit = $query->get('limit', 10);
+        $limit = $query->get('limit', 20);
         $order = $query->get('order', 'username');
         $way   = $query->get('way', 'ASC');
 
