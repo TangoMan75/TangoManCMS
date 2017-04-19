14002856255fe5c7bc6b7b7220f9763298d16c46
Matthias Morin <matthias.morin@gmail.com>
2017-04-19 17:37:29 +0200

    Uploadable

---
+++ src/AppBundle/Entity/Traits/UploadableDocument.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace AppBundle\Entity\Traits;
+
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Class UploadableDocument
+ * Requires Timestampable trait
+ *
+ * @author  Matthias Morin <tangoman@free.fr>
+ * @package AppBundle\Entity\Traits
+ */
+Trait UploadableDocument
+{
+    /**
+     * @Vich\UploadableField(mapping="documents", fileNameProperty="documentFileName")
+     * @Assert\File(maxSize="100M", mimeTypes={
+     *     "application/msword",
+     *     "application/pdf",
+     *     "application/vnd.ms-excel",
+     *     "application/vnd.ms-powerpoint",
+     *     "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+     *     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+     *     "application/zip"
+     * })
+     * @var File
+     */
+    private $documentFile;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $documentFileName;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @var integer
+     */
+    private $documentSize;
+
+    /**
+     * @return String
+     */
+    public function getDocumentFile()
+    {
+        return $this->documentFileName;
+    }
+
+    /**
+     * @param File|null $documentFile
+     *
+     * @return $this
+     */
+    public function setDocumentFile(File $documentFile = null)
+    {
+        $this->documentFile = $documentFile;
+
+        if ($documentFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->modified = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return String
+     */
+    public function getDocumentFileName()
+    {
+        return $this->documentFileName;
+    }
+
+    /**
+     * @param String $documentFileName
+     *
+     * @return $this
+     */
+    public function setDocumentFileName($documentFileName)
+    {
+        $this->documentFileName = $documentFileName;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDocumentSize()
+    {
+        return $this->documentSize;
+    }
+
+    /**
+     * @param int $documentSize
+     *
+     * @return $this
+     */
+    public function setDocumentSize($documentSize)
+    {
+        $this->documentSize = $documentSize;
+
+        return $this;
+    }
+}
---
+++ src/AppBundle/Entity/Traits/UploadableImage.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace AppBundle\Entity\Traits;
+
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Class UploadableImage
+ * Requires Timestampable trait
+ *
+ * @author  Matthias Morin <tangoman@free.fr>
+ * @package AppBundle\Entity\Traits
+ */
+Trait UploadableImage
+{
+    /**
+     * @Vich\UploadableField(mapping="upload", fileNameProperty="imageFileName")
+     * @Assert\File(maxSize="2M", mimeTypes={
+     *     "image/gif",
+     *     "image/jpeg",
+     *     "image/jpg",
+     *     "image/png"
+     * })
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $imageFileName;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @var integer
+     */
+    private $imageSize;
+
+    /**
+     * @return String
+     */
+    public function getImageFile()
+    {
+        return $this->imageFileName;
+    }
+
+    /**
+     * @param File|null $imageFile
+     *
+     * @return $this
+     */
+    public function setImageFile(File $imageFile = null)
+    {
+        $this->imageFile = $imageFile;
+
+        if ($imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->modified = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return String
+     */
+    public function getImageFileName()
+    {
+        return $this->imageFileName;
+    }
+
+    /**
+     * @param String $imageFileName
+     *
+     * @return $this
+     */
+    public function setImageFileName($imageFileName)
+    {
+        $this->imageFileName = $imageFileName;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getImageSize()
+    {
+        return $this->imageSize;
+    }
+
+    /**
+     * @param int $imageSize
+     *
+     * @return $this
+     */
+    public function setImageSize($imageSize)
+    {
+        $this->imageSize = $imageSize;
+
+        return $this;
+    }
+}
