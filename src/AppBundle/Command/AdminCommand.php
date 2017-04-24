3dccabc71cfa90a24a80f0585d6a875ecf5a6e0a
Matthias Morin <matthias.morin@gmail.com>
2017-04-24 14:48:11 +0200

    Commands

--- src/AppBundle/Command/AdminCommand.php
+++ src/AppBundle/Command/AdminCommand.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Command;
 
-
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,10 +14,8 @@ class AdminCommand extends ContainerAwareCommand
      */
     protected function configure()
     {
-        $this
-            ->setName('admin')
-            ->setDescription('Creates ROLE_SUPER_ADMIN account based on parameters.yml')
-        ;
+        $this->setName('admin')
+            ->setDescription('Creates ROLE_SUPER_ADMIN account based on parameters.yml');
     }
 
     /**
@@ -27,25 +24,27 @@ class AdminCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Creates super admin account
-        if ( !$this->getContainer()->get('em')->repository('AppBundle:User')->findByRole('ROLE_SUPER_ADMIN') ) {
+        $em = $this->getContainer()->get('doctrine')->getManager();
 
-            $email    = $this->getContainer()->getParameter('mailer_from');
+        // Creates super admin account
+        if (!$em->getRepository('AppBundle:User')->findByRole('ROLE_SUPER_ADMIN')) {
+
+            $email = $this->getContainer()->getParameter('mailer_from');
             $username = $this->getContainer()->getParameter('site_author');
-            $pwd      = $this->getContainer()->getParameter('super_admin_pwd');
+            $pwd = $this->getContainer()->getParameter('super_admin_pwd');
 
             $encoder = $this->getContainer()->get('security.password_encoder');
             $user = new User();
             $user->setUsername($username)
-                 ->setEmail($email)
-                 ->setPassword($encoder->encodePassword($user, $pwd))
-                 ->addRole('ROLE_SUPER_ADMIN')
-                 ->setBio("Ceci est le compte super administrateur.")
-            ;
+                ->setEmail($email)
+                ->setPassword($encoder->encodePassword($user, $pwd))
+                ->addRole('ROLE_SUPER_ADMIN')
+                ->setBio('Ceci est le compte super administrateur.');
 
-            $this->getContainer()->get('em')->save($user);
+            $em->persist($user);
+            $em->flush();
+
             $output->writeln('<question>'.$user->getUsername().' account created with password: "'.$pwd.'"</question>');
-
         } else {
             $output->writeln('<question>Sorry, at least one account with ROLE_SUPER_ADMIN exists already.</question>');
         }
