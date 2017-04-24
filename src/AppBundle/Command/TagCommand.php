01b187f92dc0c1cb5a2b33a1c2504f96c86407af
Matthias Morin <matthias.morin@gmail.com>
2017-04-24 14:48:45 +0200

    Commands

---
+++ src/AppBundle/Command/TagCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Tag;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TagCommand extends ContainerAwareCommand
+{
+    /**
+     * Creates command with description
+     */
+    protected function configure()
+    {
+        $this->setName('tag')
+            ->setDescription('Creates default tags');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        // Load Tags
+        $tags = [
+            'Défaut'    => 'default',
+            'Principal' => 'primary',
+            'Info'      => 'info',
+            'Succès'    => 'success',
+            'Alerte'    => 'warning',
+            'Danger'    => 'danger',
+        ];
+
+        foreach ($tags as $name => $type) {
+            // findBy is the only working method in fixtures
+            if (!$em->getRepository('AppBundle:Tag')->findBy(['name' => $name])) {
+                $tag = new Tag();
+                $tag->setName($name);
+                $tag->setType($type);
+                $tag->setReadOnly();
+                $em->persist($tag);
+                $output->writeln('<question>Tag '.$tag.' created with type'.$type.'</question>');
+            }
+        }
+
+        $em->flush();
+    }
+}
