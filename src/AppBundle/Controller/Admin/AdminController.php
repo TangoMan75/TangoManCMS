1901d89be28b42910f42d8bbd4896e259c8fe8f8
Matthias Morin <matthias.morin@gmail.com>
2017-05-06 19:39:53 +0200

    Sections Pages

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -20,11 +20,12 @@ class AdminController extends Controller
             'admin/default/index.html.twig',
             [
                 'currentUser'  => $this->getUser(),
-                'userCount'    => $this->get('doctrine')->getRepository('AppBundle:User')->count(),
+                'commentCount' => $this->get('doctrine')->getRepository('AppBundle:Comment')->count(),
+                'mediaCount'   => $this->get('doctrine')->getRepository('AppBundle:Media')->count(),
                 'pageCount'    => $this->get('doctrine')->getRepository('AppBundle:Page')->count(),
                 'postCount'    => $this->get('doctrine')->getRepository('AppBundle:Post')->count(),
-                'mediaCount'   => $this->get('doctrine')->getRepository('AppBundle:Media')->count(),
-                'commentCount' => $this->get('doctrine')->getRepository('AppBundle:Comment')->count(),
+                'sectionCount' => $this->get('doctrine')->getRepository('AppBundle:Section')->count(),
+                'userCount'    => $this->get('doctrine')->getRepository('AppBundle:User')->count(),
             ]
         );
     }
