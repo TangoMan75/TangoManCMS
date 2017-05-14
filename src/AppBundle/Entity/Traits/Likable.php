eeb903f797590a26cb3a084f22cd52371bff4a68
Matthias Morin <matthias.morin@gmail.com>
2017-05-14 17:46:57 +0200

    Likable

---
+++ src/AppBundle/Entity/Traits/Likable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Entity\Traits;
+
+/**
+ * Trait Likable
+ *
+ * @author  Matthias Morin <tangoman@free.fr>
+ * @package AppBundle\Entity\Traits
+ */
+Trait Likable
+{
+    /**
+     * @var
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $likes;
+
+    /**
+     * @return integer
+     */
+    public function getLikes()
+    {
+        return $this->likes;
+    }
+
+    /**
+     * @param integer $likes
+     * @return $this
+     */
+    public function setLikes($likes)
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+}
