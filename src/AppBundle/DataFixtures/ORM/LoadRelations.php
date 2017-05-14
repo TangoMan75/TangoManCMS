cfb51394fd2c999861079c270d212daf2c44b893
Matthias Morin <matthias.morin@gmail.com>
2017-05-14 03:12:24 +0200

    Fixtures

--- src/AppBundle/DataFixtures/ORM/LoadRelations.php
+++ src/AppBundle/DataFixtures/ORM/LoadRelations.php
@@ -44,60 +44,83 @@ class LoadRelations implements FixtureInterface, ContainerAwareInterface, Ordere
      */
     public function load(ObjectManager $em)
     {
-        // $posts = $em->getRepository('AppBundle:Post')->findBy([], null, 100);
-
         // findBy seems to be the only working method in fixtures
-        // $comments   = $em->getRepository('AppBundle:Comment')->findAll();
-        // $comment    = $em->getRepository('AppBundle:Comment')->find(1);
-        // $pages      = $em->getRepository('AppBundle:Page')->findAll();
-        // $page       = $em->getRepository('AppBundle:Page')->find(1);
-        // $posts      = $em->getRepository('AppBundle:Post')->findAll();
-        // $post       = $em->getRepository('AppBundle:Post')->find(1);
-        // $privileges = $em->getRepository('AppBundle:Privilege')->findAll();
-        // $privilege  = $em->getRepository('AppBundle:Privilege')->find(1);
-        // $roles      = $em->getRepository('AppBundle:Role')->findAll();
-        // $role       = $em->getRepository('AppBundle:Role')->find(1);
-        // $sections   = $em->getRepository('AppBundle:Section')->findAll();
-        // $section    = $em->getRepository('AppBundle:Section')->find(1);
-        // $tags       = $em->getRepository('AppBundle:Tag')->findAll();
-        // $tag        = $em->getRepository('AppBundle:Tag')->find(1);
-        // $users      = $em->getRepository('AppBundle:User')->findAll();
-        // $user       = $em->getRepository('AppBundle:User')->find(1);
+        $comments   = $em->getRepository('AppBundle:Comment')->findAll();
+        $pages      = $em->getRepository('AppBundle:Page')->findAll();
+        $posts      = $em->getRepository('AppBundle:Post')->findAll();
+        $privileges = $em->getRepository('AppBundle:Privilege')->findAll();
+        $roles      = $em->getRepository('AppBundle:Role')->findAll();
+        $sections   = $em->getRepository('AppBundle:Section')->findAll();
+        $tags       = $em->getRepository('AppBundle:Tag')->findAll();
+        $users      = $em->getRepository('AppBundle:User')->findAll();
 
-        // $comment->setPost($posts[mt_rand(1, count($posts) - 1)]);
-        // $comment->setUser($users[mt_rand(1, count($users) - 1)]);
-        // $em->persist($comment);
+        // Comments
+        foreach ($comments as $comment) {
+            $comment->setPost($posts[mt_rand(1, count($posts) - 1)]);
+            $comment->setUser($users[mt_rand(1, count($users) - 1)]);
+            $em->persist($comment);
+        }
+        $em->flush();
 
-        // $page->addSection($sections[mt_rand(1, count($sections) - 1)]);
-        // $em->persist($page);
+        // Pages
+        foreach ($pages as $page) {
+            $page->addSection($sections[mt_rand(1, count($sections) - 1)]);
+            $em->persist($page);
+        }
+        $em->flush();
 
-        // $post->addComment($comment[mt_rand(1, count($comment) - 1)]);
-        // $post->addSection($sections[mt_rand(1, count($sections) - 1)]);
-        // $post->addTag($tags[mt_rand(1, count($tags) - 1)]);
-        // $post->setUser($users[mt_rand(1, count($users) - 1)]);
-        // $em->persist($post);
+        // Posts
+        foreach ($posts as $post) {
+            $post->addComment($comments[mt_rand(1, count($comments) - 1)]);
+            $post->addSection($sections[mt_rand(1, count($sections) - 1)]);
+            // shuffle($tags);
+            // for ($i = 0; $i < mt_rand(0, 4); $i++){
+            //     $post->addTag($tags[$i]);
+            // }
+            $post->setUser($users[mt_rand(1, count($users) - 1)]);
+            $em->persist($post);
+        }
+        $em->flush();
 
-        // $privilege->addRole($roles[mt_rand(1, count($roles) - 1)]);
-        // $em->persist($privilege);
-
-        // $role->addPrivilege($privileges[mt_rand(1, count($privileges) - 1)]);
-        // // $role->addUser($users[mt_rand(1, count($users) - 1)]);
-        // $em->persist($role);
-
-        // $section->addPage($pages[mt_rand(1, count($pages) - 1)]);
-        // $section->addPost($posts[mt_rand(1, count($posts) - 1)]);
-        // $section->addTag($tags[mt_rand(1, count($tags) - 1)]);
-        // $em->persist($section);
-
-        // $tag->addPage($pages[mt_rand(1, count($pages) - 1)]);
-        // $tag->addPost($posts[mt_rand(1, count($posts) - 1)]);
-        // $tag->addSection($sections[mt_rand(1, count($sections) - 1)]);
-        // $em->persist($tag);
-
-        // $user->addPost($posts[mt_rand(1, count($posts) - 1)]);
-        // // $user->addRole($roles[mt_rand(1, count($roles) - 1)]);
-        // $em->persist($user);
-
+        // // Privileges
+        // foreach ($privileges as $privilege) {
+        //     $privilege->addRole($roles[mt_rand(1, count($roles) - 1)]);
+        //     $em->persist($privilege);
+        // }
         // $em->flush();
+
+        // // Roles
+        // foreach ($roles as $role) {
+        //     $role->addPrivilege($privileges[mt_rand(1, count($privileges) - 1)]);
+        //     // $role->addUser($users[mt_rand(1, count($users) - 1)]);
+        //     $em->persist($role);
+        // }
+        // $em->flush();
+
+        // // Sections
+        // foreach ($sections as $section) {
+        //     $section->addPage($pages[mt_rand(1, count($pages) - 1)]);
+        //     $section->addPost($posts[mt_rand(1, count($posts) - 1)]);
+        //     // $section->addTag($tags[mt_rand(1, count($tags) - 1)]);
+        //     $em->persist($section);
+        // }
+        // $em->flush();
+
+        // // Tags
+        // foreach ($tags as $tag) {
+        //     $tag->addItem($pages[mt_rand(1, count($pages) - 1)]);
+        //     $tag->addItem($posts[mt_rand(1, count($posts) - 1)]);
+        //     $tag->addItem($sections[mt_rand(1, count($sections) - 1)]);
+        //     $em->persist($tag);
+        // }
+        // $em->flush();
+
+        // Users
+        foreach ($users as $user) {
+            $user->addPost($posts[mt_rand(1, count($posts) - 1)]);
+            // $user->addRole($roles[mt_rand(1, count($roles) - 1)]);
+            $em->persist($user);
+        }
+        $em->flush();
     }
 }
