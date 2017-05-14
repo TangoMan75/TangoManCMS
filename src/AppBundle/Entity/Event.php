281ba07ea0d3c025eaca041a934886a5286269ec
Matthias Morin <matthias.morin@gmail.com>
2017-05-14 17:45:22 +0200

    Events

---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Event
+ *
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="event")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
+ * @author  Matthias Morin <tangoman@free.fr>
+ * @package AppBundle\Entity
+ */
+class Event
+{
+    use Traits\HasSummary;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime Event Timestamp
+     * @ORM\Column(type="datetime")
+     */
+    private $timestamp;
+
+    /**
+     * Event constructor.
+     */
+    public function __construct()
+    {
+        $this->timestamp = new \DateTimeImmutable();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param \DateTime $timestamp
+     *
+     * @return $this
+     */
+    public function setTimestamp(\DateTime $timestamp)
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->summary;
+    }
+}
