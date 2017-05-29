d5f5cf935e272881ed5088496400e49d44e44647
Matthias Morin <matthias.morin@gmail.com>
2017-05-29 17:58:22 +0200

    Feature: Galleries

---
+++ src/AppBundle/Controller/Admin/GalleryController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\Section;
+use AppBundle\Form\Admin\AdminNewGalleryType;
+use AppBundle\Form\Admin\AdminEditGalleryType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class GalleryController
+ * @Route("/admin/galleries")
+ *
+ * @package AppBundle\Controller
+ */
+class GalleryController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction(Request $request)
+    {
+        // Show searchable, sortable, paginated gallery list
+        $em = $this->get('doctrine')->getManager();
+        $galleries = $em->getRepository('AppBundle:Section')->searchableOrderedPage($request->query);
+
+        return $this->render(
+            'admin/gallery/index.html.twig',
+            [
+                'currentUser' => $this->getUser(),
+                'galleries'    => $galleries,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/new")
+     */
+    public function newAction(Request $request)
+    {
+        $gallery = new Section();
+        $form = $this->createForm(AdminNewGalleryType::class, $gallery);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Persists new gallery
+            $em = $this->get('doctrine')->getManager();
+            $em->persist($gallery);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'La galerie <strong>&quot;'.$gallery.'&quot;</strong> a bien été ajoutée.');
+
+            // User is redirected to referrer gallery
+            return $this->redirect($request->get('callback'));
+        }
+
+        return $this->render(
+            'admin/gallery/new.html.twig',
+            [
+                'currentUser' => $this->getUser(),
+                'form'        => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/publish/{id}/{publish}", requirements={"id": "\d+", "publish": "\d+"})
+     */
+    public function publishAction(Request $request, Gallery $gallery, $publish)
+    {
+        $gallery->setPublished($publish);
+        $em = $this->get('doctrine')->getManager();
+        $em->persist($gallery);
+        $em->flush();
+
+        if ($publish) {
+            $message = 'La gallery <strong>&quot;'.$gallery.'&quot;</strong> a bien été publiée.';
+        } else {
+            $message = 'La publication de la galerie <strong>&quot;'.$gallery.'&quot;</strong> a bien été annulée.';
+        }
+
+        // Send flash notification
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            $message
+        );
+
+        // User is redirected to referrer page
+        return $this->redirect($request->get('callback'));
+    }
+
+    /**
+     * @Route("/edit/{id}", requirements={"id": "\d+"})
+     */
+    public function editAction(Request $request, Gallery $gallery)
+    {
+        $form = $this->createForm(AdminEditGalleryType::class, $gallery);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Persists edited gallery
+            $em = $this->get('doctrine')->getManager();
+            $em->persist($gallery);
+            $em->flush();
+            // Displays success message
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                'La galerie <strong>&quot;'.$gallery.'&quot;</strong> a bien été modifiée.'
+            );
+
+            // User is redirected to referrer page
+            return $this->redirect($request->get('callback'));
+        }
+
+        return $this->render(
+            'admin/gallery/edit.html.twig',
+            [
+                'currentUser' => $this->getUser(),
+                'form'        => $form->createView(),
+                'gallery'     => $gallery,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/delete/{id}", requirements={"id": "\d+"})
+     */
+    public function deleteAction(Request $request, Gallery $gallery)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_SUPER_ADMIN')) {
+            $this->get('session')->getFlashBag()->add(
+                'error',
+                'Désolé, <strong>'.$this->getUser().'</strong><br />'.
+                'Vous n\'êtes pas autorisé à effectuer cette action.'
+            );
+
+            // User is redirected to referrer gallery
+            return $this->redirect($request->get('callback'));
+        }
+
+        // Deletes specified gallery
+        $em = $this->get('doctrine')->getManager();
+        $em->remove($gallery);
+        $em->flush();
+
+        // Send flash notification
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            'La galerie <strong>&quot;'.$gallery.'&quot;</strong> a bien été supprimée.'
+        );
+
+        // User is redirected to referrer page
+        return $this->redirect($request->get('callback'));
+    }
+}
---
+++ src/AppBundle/Form/Admin/AdminNewGalleryType.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace AppBundle\Form\Admin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
+
+class AdminNewGalleryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'title',
+                TextType::Class,
+                [
+                    'label' => 'Titre',
+                ]
+            )
+            ->add(
+                'tags',
+                EntityType::class,
+                [
+                    'label'         => 'Étiquette',
+                    'class'         => 'AppBundle:Tag',
+                    // 'empty_data' => null,
+                    'multiple'      => true,
+                    'expanded'      => true,
+                    'required'      => false,
+                    'query_builder' => function (EntityRepository $em) {
+                        return $em->createQueryBuilder('tag');
+                    },
+                ]
+            )
+            ->add(
+                'pages',
+                EntityType::class,
+                [
+                    'label'         => 'Pages',
+                    'class'         => 'AppBundle:Page',
+                    'by_reference'  => false,
+                    // 'empty_data' => null,
+                    'multiple'      => true,
+                    'expanded'      => false,
+                    'required'      => false,
+                    'query_builder' => function (EntityRepository $em) {
+                        return $em->createQueryBuilder('page')
+                            ->orderBy('page.title');
+                    },
+                ]
+            )
+            ->add(
+                'posts',
+                EntityType::class,
+                [
+                    'label'         => 'Médias',
+                    'class'         => 'AppBundle:Post',
+                    'by_reference'  => false,
+                    // 'empty_data' => null,
+                    'multiple'      => true,
+                    'expanded'      => false,
+                    'required'      => false,
+                    'query_builder' => function (EntityRepository $em) {
+                        return $em->createQueryBuilder('post')
+                            ->orderBy('post.title');
+                    },
+                ]
+            )
+            ->add(
+                'published',
+                CheckboxType::class,
+                [
+                    'label'    => 'Publier',
+                    'required' => false,
+                ]
+            );
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'AppBundle\Entity\Section',
+            ]
+        );
+    }
+
+    public function getName()
+    {
+        return 'gallery';
+    }
+}
