3fca99674ae82c678aba1538f9d3a61ec9f96417
Matthias Morin <matthias.morin@gmail.com>
2017-06-20 18:25:45 +0200

    Services: StingHelperService

---
+++ src/AppBundle/Service/StringHelperService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Service;
+
+/**
+ * @package AppBundle\Services
+ */
+class StringHelperService
+{
+    /**
+     * Generates slug from string
+     *
+     * @param  String  $string                       Source string
+     * @param  Boolean $removeWesternEuropeanAccents [optional] When true, removes western european accents (ex. é => e) - default: true
+     * @param  String  $separator                    [optional] Character to use to replace non-alphanum chars - default: '-'
+     *
+     * @return String  clean string
+     */
+    public function slugify($string, $removeWesternEuropeanAccents = true, $separator = '-')
+    {
+        $slug = trim($string);
+        if ($removeWesternEuropeanAccents) {
+            // That may seem weird but this seems to be the better way to do it
+            $slug = htmlentities($slug, ENT_NOQUOTES, 'UTF-8');
+            $slug = preg_replace('/&#?([a-zA-Z])[a-zA-Z0-9]*;/i', '${1}', $slug);
+        }
+        // Convert string
+        $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $slug);
+        // Remove illegal characters
+        $slug = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $slug);
+        $slug = mb_strtolower(trim($slug, $separator), 'UTF-8');
+        // Replace illegal characters
+        $slug = preg_replace("/[\/_|+ -]+/", $separator, $slug);
+
+        return $slug;
+    }
+}
