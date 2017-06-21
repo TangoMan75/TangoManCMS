b6781fdf993ee023c923989596f7945edaecba6e
Matthias Morin <matthias.morin@gmail.com>
2017-06-21 16:36:44 +0200

    B.O: Admin Default

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -19,12 +19,42 @@ class AdminController extends Controller
         return $this->render(
             'admin/default/index.html.twig',
             [
-                'commentCount' => $this->get('doctrine')->getRepository('AppBundle:Comment')->count(),
-                'mediaCount'   => null,
-                'pageCount'    => $this->get('doctrine')->getRepository('AppBundle:Page')->count(),
-                'postCount'    => $this->get('doctrine')->getRepository('AppBundle:Post')->count(),
-                'sectionCount' => $this->get('doctrine')->getRepository('AppBundle:Section')->count(),
-                'userCount'    => $this->get('doctrine')->getRepository('AppBundle:User')->count(),
+                'commentCount'   => $this->get('doctrine')->getRepository('AppBundle:Comment')->count(),
+                'pageCount'      => $this->get('doctrine')->getRepository('AppBundle:Page')->count(),
+                'postCount'      => $this->get('doctrine')->getRepository('AppBundle:Post')->count(['type' => 'post']),
+                'mediaCount'     => $this->get('doctrine')->getRepository('AppBundle:Post')->count(
+                    [
+                        'type' => [
+                            '360',
+                            'argus360',
+                            'csv',
+                            'dailymotion',
+                            'doc',
+                            'document',
+                            'embed',
+                            'gif',
+                            'giphy',
+                            'gist',
+                            'image',
+                            'jpeg',
+                            'jpg',
+                            'ods',
+                            'odt',
+                            'pdf',
+                            'png',
+                            'pptx',
+                            'thetas',
+                            'tweet',
+                            'txt',
+                            'vimeo',
+                            'xls',
+                            'youtube',
+                        ],
+                    ]
+                ),
+                'galleryCount'   => $this->get('doctrine')->getRepository('AppBundle:Section')->count(['type' => 'gallery']),
+                'sectionCount'   => $this->get('doctrine')->getRepository('AppBundle:Section')->count(['type' => 'section']),
+                'userCount'      => $this->get('doctrine')->getRepository('AppBundle:User')->count(),
             ]
         );
     }
