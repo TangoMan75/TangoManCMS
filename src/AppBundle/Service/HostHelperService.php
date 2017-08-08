05a669fbb03c5a71913a0a11b804cc3659c05223
Matthias Morin <matthias.morin@gmail.com>
2017-08-08 12:02:17 +0200

    Service: HostHelper

---
+++ src/AppBundle/Service/HostHelperService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Service;
+
+/**
+ * @package AppBundle\Services
+ */
+class HostHelperService
+{
+    /**
+     * @return string
+     */
+    public function getDomainLabel()
+    {
+        return explode('.', $_SERVER["SERVER_NAME"])[0];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isApi()
+    {
+        if ($this->getDomainLabel() == 'api') {
+            return true;
+        }
+
+        return false;
+    }
+}
