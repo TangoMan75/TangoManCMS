a3038f0ffc9b1c7ec232244abbf6c52e111fb0c1
Matthias Morin <matthias.morin@gmail.com>
2017-08-09 10:42:12 +0200

    Bundles: DependencyInjectino

--- src/TangoMan/CallbackBundle/DependencyInjection/Configuration.php
+++ src/TangoMan/CallbackBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('tangoman_callback');
+        $rootNode = $treeBuilder->root('tangoman_callback');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
