c474b7aaab659bcd2a9548f13420cc56e67f19ba
Matthias Morin <matthias.morin@gmail.com>
2017-08-17 11:24:51 +0200

    Bundle

--- src/AppBundle/Entity/Privilege.php
+++ src/AppBundle/Entity/Privilege.php
@@ -5,7 +5,6 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use TangoMan\UserBundle\Model\Privilege as TangoManPrivilege;
 
 /**
  * Class Privilege
@@ -16,13 +15,62 @@ use TangoMan\UserBundle\Model\Privilege as TangoManPrivilege;
  * @author  Matthias Morin <tangoman@free.fr>
  * @package AppBundle\Entity
  */
-class Privilege extends TangoManPrivilege
+class Privilege
 {
+    use Relationships\PrivilegesHaveRoles;
+    use Relationships\PrivilegesHaveUsers;
+
+    use Traits\HasLabel;
+    use Traits\HasName;
+    use Traits\HasType;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
     /**
      * Privilege constructor.
      */
     public function __construct()
     {
-        parent::__construct();
+        $this->roles = new ArrayCollection();
+        $this->users = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\PreFlush()
+     */
+    public function setDefaults()
+    {
+        // Default privilege type is uppercased name with "CAN_" prefix without whitespaces
+        if (!$this->type) {
+            $this->type = $this->name;
+        }
+
+        $this->type = strtoupper(str_replace(' ', '_', $this->type));
+
+        if (stripos($this->type, 'CAN_') !== 0) {
+            $this->type = 'CAN_'.$this->type;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->type;
     }
 }
--- src/AppBundle/Entity/Role.php
+++ src/AppBundle/Entity/Role.php
@@ -3,11 +3,9 @@
 namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\Authorization\Voter\RoleHierarchyVoter;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\Role\RoleInterface;
-
-use TangoMan\UserBundle\Model\Role as TangoManRole;
 
 /**
  * Class Role
@@ -18,13 +16,115 @@ use TangoMan\UserBundle\Model\Role as TangoManRole;
  * @author  Matthias Morin <tangoman@free.fr>
  * @package AppBundle\Entity
  */
-class Role extends TangoManRole
+class Role implements RoleInterface
 {
+    use Relationships\RolesHavePrivileges;
+    use Relationships\RolesHaveUsers;
+
+    use Traits\HasLabel;
+    use Traits\HasName;
+    use Traits\HasType;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $icon;
+
     /**
      * Role constructor.
      */
     public function __construct()
     {
-        parent::__construct();
+        $this->privileges = new ArrayCollection();
+        $this->users = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIcon()
+    {
+        if (!$this->icon) {
+            return 'glyphicon glyphicon-user';
+        } else {
+            return $this->icon;
+        }
+    }
+
+    /**
+     * @param string $icon
+     *
+     * @return Role
+     */
+    public function setIcon($icon)
+    {
+        // Allowed icons for user roles are: User, pawn, knight, bishop, tower, queen, and king.
+        if (in_array(
+            $icon, [
+                     'glyphicon glyphicon-user',
+                     'glyphicon glyphicon-pawn',
+                     'glyphicon glyphicon-knight',
+                     'glyphicon glyphicon-bishop',
+                     'glyphicon glyphicon-tower',
+                     'glyphicon glyphicon-queen',
+                     'glyphicon glyphicon-king',
+                 ]
+        )) {
+            $this->icon = $icon;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PreFlush()
+     */
+    public function setDefaults()
+    {
+        // Default role type is uppercased name with "ROLE_" prefix without whitespaces
+        if (!$this->type) {
+            $this->type = $this->name;
+        }
+
+        $this->type = strtoupper(str_replace(' ', '_', $this->type));
+
+        if (stripos($this->type, 'ROLE_') !== 0) {
+            $this->type = 'ROLE_'.$this->type;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->type;
     }
 }
