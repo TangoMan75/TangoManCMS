43b4de58c38d6b2c2107c4b518714baa703e72f0
MORIN Matthias <matthias.morin@gmail.com>
2017-10-16 18:00:39 +0200

    Callbacks

---
+++ src/AppBundle/Command/DeployCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DeployCommand extends ContainerAwareCommand
+{
+    /**
+     * Configuration
+     */
+    protected function configure()
+    {
+        $this->setName('deploy')
+            ->setDescription('Run script after deploying application');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("<question>Deploying app</question>");
+
+        // Update database schema
+        $output->writeln("<comment>Updating database schema</comment>");
+        $command = $this->getApplication()->find('doctrine:schema:update');
+        $arguments = ['--force' => true];
+        $greetInput = new ArrayInput($arguments);
+        $command->run($greetInput, $output);
+
+        // Deploying dev environment
+        if ($this->getContainer()->getParameter('symfony_env') == 'dev') {
+            $output->writeln("<question>Deploying dev environment</question>");
+            // Fixture
+            $output->writeln("<comment>Loading fixtures</comment>");
+            $command = $this->getApplication()->find('doctrine:fixture:load');
+            $arguments = [];
+            $greetInput = new ArrayInput($arguments);
+            $greetInput->setInteractive(false);
+            $command->run($greetInput, $output);
+        }
+
+        $output->writeln("<question>Application deployed</question>");
+    }
+}
