fcd71df4558ea8dccb1823b73cbc2897307284cc
Matthias Morin <matthias.morin@gmail.com>
2018-04-09 00:51:36 +0200

    fix composer clear cache

--- src/TangoMan/TestBundle/Controller/RecursionTestController.php
+++ src/TangoMan/TestBundle/Controller/RecursionTestController.php
@@ -15,7 +15,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-
 /**
  * Class RecursionTestController
  *
